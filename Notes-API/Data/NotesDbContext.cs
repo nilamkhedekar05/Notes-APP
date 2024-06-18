44756fcb6cc0647f9d20322c7c2fb039edc00f89
nilamkhedekar05 <90793313+nilamkhedekar05@users.noreply.github.com>
2024-06-18 14:41:02 +0530

    Add files via upload

---
+++ Notes-API/Data/NotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes_API.Models.Entities;
+
+namespace Notes_API.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}
